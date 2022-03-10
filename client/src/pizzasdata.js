a52266a9d3edffd7ecd3a832adf3df7dc58ddd3d
Aekkasitrun <aekkasitrun@gmail.com>
2022-03-10 10:44:19 +0700

    add data_food

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,21 @@
+const pizzas =[
+    {
+
+        "name": "PEPPER BARBECUE CHICKEN",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [{
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+        ],
+        "category": "nonveg",
+        "image": "https://i0.wp.com/chefchoice.com.np/wp-content/uploads/2020/07/Pepper_Barbeque.jpg?fit=267%2C266&ssl=1",
+        "description": "Pepper Barbeque Chicken I Cheese"
+    }
+]
+export default pizzas
