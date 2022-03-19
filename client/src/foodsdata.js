e198e6480863bc83e006715d7f81361d4a8d45b6
Aekkasitrun <aekkasitrun@gmail.com>
2022-03-19 19:40:24 +0700

    add food in fooddata

--- client/src/foodsdata.js
+++ client/src/foodsdata.js
@@ -16,6 +16,25 @@ const foods =[
         "category": "nonveg",
         "image": "https://i0.wp.com/chefchoice.com.np/wp-content/uploads/2020/07/Pepper_Barbeque.jpg?fit=267%2C266&ssl=1",
         "description": "Pepper Barbeque Chicken I Cheese"
+    },
+
+    {
+
+        "name": "Tom Yum Goong",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [{
+            "small": 40,
+            "medium": 60,
+            "large": 80
+        }
+        ],
+        "category": "nonveg",
+        "image": "https://farm8.staticflickr.com/7227/7353291288_0a8048bb80_o.jpg",
+        "description": "The clear version is known as tom yum nam sai "
     }
 ]
 export default foods
