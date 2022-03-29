d426b9d44032267bf36c4a8ce1475bfdf9c1f344
Aorts <gusgame.aor@gmail.com>
2022-03-29 14:27:50 +0700

    db

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-var mongoURL = 'mongodb+srv://ts:aor@cluster0.qhmk7.mongodb.net/test'
+var mongoURL = 'mongodb+srv://ts:aor@cluster0.qhmk7.mongodb.net/dcw'
 
 mongoose.connect(mongoURL, {useUnifiedTopology:true , useNewUrlParser:true})
 
 var db = mongoose.connection
 
-db.on('connection', ()=>{
+db.on('connected', ()=>{
     console.log('database connection successfull')
 })
 
