96721499d6ab15d745d16a8392aa4285e33af08e
Aorts <gusgame.aor@gmail.com>
2022-03-31 19:37:37 +0700

    cart setting

--- client/src/screens/Cartscreen.js
+++ client/src/screens/Cartscreen.js
@@ -4,25 +4,35 @@ import { useSelector, useDispatch } from 'react-redux'
 export default function Cartscreen() {
     const cartstate = useSelector(state => state.addToCartReducer)
     const cartItems = cartstate.cartItems
+    var subtotal = cartItems.reduce((x, item) => x + item.price, 0)
     return (<div>
         <div className="row justify-content-center">
 
             <div className="col-md-6">
                 <h2 style={{ fontSize: '40px' }}>
-                    My Cart {cartItems.length}
+                    My Cart
                 </h2>
                 {cartItems.map(item =>
-                        <div className="flex-container">
-                            <div>
-                                <h1>
-                                    {item.name}
-                                </h1>
-                                <h1>
-                                    {item.price}
-                                </h1>
-                            </div>
+                    <div className="flex-container">
+
+
+                        <div className="text-left m-1 w-100">
+                            <h1>
+                                {item.name} [{item.varient}]
+                            </h1>
+                            <h1>
+                                Price : {item.quantity} * {item.prices[0][item.varient]} = {item.price} THB
+                            </h1>
+                            <hr />
                         </div>
+
+                    </div>
                 )}
+
+                <div className="text-left m-1 w-100">
+                    <h2 style={{ fontSize: '40px' }}>subtotal = {subtotal} THB</h2>
+
+                </div>
             </div>
 
         </div>
