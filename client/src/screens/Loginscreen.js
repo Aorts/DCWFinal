7c86804b3d64d07f35b023d5485388028b2aaee1
Aekkasitrun <aekkasitrun@gmail.com>
2022-04-01 18:28:45 +0700

    decorate register page

--- client/src/screens/Loginscreen.js
+++ client/src/screens/Loginscreen.js
@@ -1,11 +1,15 @@
-import React , {useState, useEffect} from "react";
-import { useDispatch , useSeclector } from "react-redux";
+import React , {useState, useEffect} from 'react';
+import { useDispatch , useSeclector } from 'react-redux'
 import {loginUser} from "../action/userActions";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
 
 export default function Loginscreen() {
 
     const [email, setemail] = useState("");
     const [password, setpassword] = useState("");
+    //const loginstate = useSeclector(state =>state.loginUserReducer)
+    //const {loading , error} = loginstate
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -25,7 +29,7 @@ export default function Loginscreen() {
     return(
         <div>
             <div className="row justify-content-center mt-5">
-                <div className="col-md-5 mt-5 text-left">
+                <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
                     <h2 className="text-center m-2" style={{fontSize: "35px"}}>
                         Login
                     </h2>
@@ -40,8 +44,9 @@ export default function Loginscreen() {
                             onChange={(e)=>{setpassword(e.target.value)}}    
                         />
 
-                        <button onClick={login} className="btn mt-3">LOGIN</button>
-                        <a href="/register">Click Here To Register</a>
+                        <button onClick={login} className="btn mt-3 mb-3">LOGIN</button>
+                        <br/>
+                        <a style={{color:'black'}} href="/register" className="mt-2">Click Here To Register</a>
                     </div>
                 </div>
 
