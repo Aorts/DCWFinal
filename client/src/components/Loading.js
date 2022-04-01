c9dcaf80b1e4b983f56102bdd13c8b4c4c2b66f9
Aorts <gusgame.aor@gmail.com>
2022-04-02 04:49:29 +0700

    fix wraning

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -5,7 +5,7 @@ export default function Loading() {
         <div>
 
             <div className="spinner-grow text-danger" role="status" style={{ height: '100px', width: '100px', marginTop: '100px' }}>
-                <span class="sr-only">Loading...</span>
+                <span className="sr-only">Loading...</span>
             </div>
 
         </div>
