1b1e9208d7f5b845ceba130172b88e8d9185d5c7
Aorts <gusgame.aor@gmail.com>
2022-04-02 06:08:33 +0700

    show a user name

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,16 +5,14 @@ import { logoutUser } from '../action/userActions';
 
 export default function Navbar() {
     const cartstate = useSelector(state => state.addToCartReducer)
-    const cartItems = cartstate.cartItems
+    const userstate = useSelector(state => state.loginUserReducer)
     const dispatch = useDispatch()
 
-    // const userstate = useSelector(state => state.loginUserReducer)
     //const currentUser = userstate
+    const currentUser = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : null
 
     return (
         <div>
-
-
 
             <nav className="navbar navbar-expand-lg shadow-lg p-3 bg-white rounded">
                 <div className='container-fluidit'>
@@ -22,42 +20,43 @@ export default function Navbar() {
                         ChangFood
                     </a>
                 </div>
-
-
-
-
                 <div className="navbar-collapse" id="navbarNav">
                     <div class="d-flex justify-content-end"> <div class="mr-auto p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
-                    <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+                        <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
                         <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
                         <div class="d-flex justify-content-end"> <div class="mr-auto p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
-                    <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
-                        <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div> 
-                    </div>
-                    <div class="d-flex justify-content-end"> <div class="mr-auto p-16">  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
-                    <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
-                        <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div> 
-                    </div>
-                    <ul className="navbar-nav mr p-2">
+                            <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+                            <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+                        </div>
+                        <div class="d-flex justify-content-end"> <div class="mr-auto p-16">  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+                            <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+                            <div class="p-16">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+                        </div>
+                        <ul className="navbar-nav mr p-2">
 
+                            {currentUser ? (
+                            
+                                <li className="nav-item">
+                                    <a>
+                                    USERNAME&nbsp;&nbsp;:&nbsp;&nbsp;{currentUser.name}&nbsp;&nbsp;&nbsp;&nbsp;
+                                    </a>
+                                    <a className="nav-link text-center" onClick={() => { dispatch(logoutUser()) }}>
+                                    Logout
+                                    </a>
+                                </li>) : (
+                                <li className="nav-item">
+                                    <a className="nav-link text-center" href="/login">
+                                        Login
+                                    </a>
 
-                        <li className="nav-item">
-                            <a className="nav-link text-center" href="/login">
-                                Login
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" href="/cart">
-                                Cart {cartItems.length}
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <i className="fa fa-sign-out fa-2x mt-1 " aria-hidden="true" onClick={() => { dispatch(logoutUser()) }}></i>
-                        </li>
-
-                    </ul>
+                                </li>
+                            )}
+                            <li className="nav-item">
+                                <a className="nav-link" href="/cart">
+                                    Cart&nbsp;{cartstate.cartItems.length}
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
